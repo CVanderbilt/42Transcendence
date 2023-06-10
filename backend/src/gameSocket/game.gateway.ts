20288086354702cc275d150e3b203516877bf67b
Eduardo <eduhgb5198@gmail.com>
2023-06-10 12:40:21 +0200

    ballpos update sin lag

--- backend/src/gameSocket/game.gateway.ts
+++ backend/src/gameSocket/game.gateway.ts
@@ -100,37 +100,47 @@ export class GameGateway
             const paddle1 = _room.player1;
             const paddle2 = _room.player2;
 
-            if (_room.ballpos.x + _room.ballpos.dx < ballMinX) {
-              if (_room.ballpos.y > paddle1.paddlePos && _room.ballpos.y < paddle1.paddlePos + paddle1.paddleHeight) {
-                if (paddle1.downPressed && _room.ballpos.dy < 4) {
-                  _room.ballpos.dy++;
+            if (_room.ballpos.x + _room.ballpos.dx < 15) {
+              {
+                if (_room.ballpos.y > _room.player1.paddlePos && _room.ballpos.y < _room.player1.paddlePos + _room.player1.paddleHeight) {
+                  if (_room.player1.downPressed && _room.ballpos.dy < 4) {
+                    _room.ballpos.dy++
+                  }
+                  if (_room.player1.upPressed && _room.ballpos.dy > -4) {
+                    _room.ballpos.dy--
+                  }
+                  _room.ballpos.dx = 2;
+                } else if (_room.ballpos.x + _room.ballpos.dx < 0) {
+                  _room.player2.score++
+                  this.matchesService.opponentGoal(room)
+                  console.log("GOL1")
+                  _room.ballpos.x = 250;
+                  if (_room.ballpos.dy > 0)
+                  _room.ballpos.dy = 2
+                  else
+                  _room.ballpos.dy = -2
                 }
-                if (paddle1.upPressed && _room.ballpos.dy > -4) {
-                  _room.ballpos.dy--;
-                }
-                _room.ballpos.dx = 2;
-              } else if (_room.ballpos.x + _room.ballpos.dx < 0) {
-                _room.player2.score++;
-                this.matchesService.opponentGoal(room);
-                console.log("GOL1");
-                _room.ballpos.x = 250;
-                _room.ballpos.dy = (_room.ballpos.dy > 0) ? 2 : -2;
               }
-            } else if (_room.ballpos.x + _room.ballpos.dx > ballMaxX) {
-              if (_room.ballpos.y > paddle2.paddlePos && _room.ballpos.y < paddle2.paddlePos + paddle2.paddleHeight) {
-                if (paddle2.downPressed && _room.ballpos.dy < 4) {
-                  _room.ballpos.dy++;
+            } else if (_room.ballpos.x + _room.ballpos.dx > canvasWidth - 15) {
+              if (_room.ballpos.y > _room.player2.paddlePos && _room.ballpos.y < _room.player2.paddlePos + _room.player2.paddleHeight) {
+                if (_room.player2.downPressed && _room.ballpos.dy < 4) {
+                  _room.ballpos.dy++
                 }
-                if (paddle2.upPressed && _room.ballpos.dy > -4) {
-                  _room.ballpos.dy--;
+                if (_room.player2.upPressed && _room.ballpos.dy > -4) {
+                  _room.ballpos.dy--
                 }
                 _room.ballpos.dx = -2;
-              } else if (_room.ballpos.x + _room.ballpos.dx > canvasWidth) {
-                _room.ballpos.x = 250;
+              } else if (_room.ballpos.x + _room.ballpos.dx > canvasWidth - 0) {
+                _room.ballpos.x = 250
                 _room.player1.score++;
                 this.matchesService.localGoal(room);
-                console.log("GOL2");
-                _room.ballpos.dy = (_room.ballpos.dy > 0) ? 2 : -2;
+                console.log("GOL2")
+                if (_room.ballpos.dy > 0)
+                _room.ballpos.dy = 2
+                else
+                _room.ballpos.dy = -2
+                //alert("GAME OVER");
+                //document.location.reload();
               }
             }
             _room.ballpos.x += _room.ballpos.dx;
