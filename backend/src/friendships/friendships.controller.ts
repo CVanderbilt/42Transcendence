f23ccaf7bd07bcd848fe66f115d74d737bb63c0f
Eduardo <eduhgb5198@gmail.com>
2023-06-24 01:08:05 +0200

    Validation en friendships.controller

--- backend/src/friendships/friendships.controller.ts
+++ backend/src/friendships/friendships.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Delete, Get, Logger, Param, Post } from '@nestjs/common';
-import { validateInput } from 'src/utils/utils';
+import { ID_VALIDATOR, validateInput } from 'src/utils/utils';
 import { FriendshipDto } from './friendships.dtos';
 import { FriendshipsService } from './friendships.service';
 import * as Joi from 'joi'
@@ -10,14 +10,17 @@ export class FriendshipsController {
 
     @Get("/user/:userId")
     getFriendships(@Param('userId') userId: string) : Promise<FriendshipDto[]> {
+        validateInput(Joi.object({
+            userId: ID_VALIDATOR.required(),
+        }), { userId });
         return this.friendsipsService.getUserFriendships(userId);
     }
 
     @Post()
     createFriendship(@Body() data: FriendshipDto) {
         validateInput(Joi.object({
-            userId: Joi.string().guid().required(),
-            friendId: Joi.string().guid().required(),
+            userId: ID_VALIDATOR.required(),
+            friendId: ID_VALIDATOR.required(),
         }), data);
         Logger.log(data);
         return this.friendsipsService.createFriendship(data);
@@ -26,13 +29,17 @@ export class FriendshipsController {
     @Post("/:friendshipId")
     updateFriendship(@Param('friendshipId') friendshipId: string, @Body() data: FriendshipDto) {
         validateInput(Joi.object({
-            isBlocked: Joi.boolean().required(),
-        }), data);
+            friendshipId: ID_VALIDATOR.required(),
+            isBlocked: Joi.boolean().required()
+        }), {...data, friendshipId });
         return this.friendsipsService.updateFriendship(friendshipId, data.isBlocked);
     }
 
     @Delete("/:friendshipId")
     deleteFriendship(@Param('friendshipId') friendshipId: string) { 
+        validateInput(Joi.object({
+            friendshipId: ID_VALIDATOR.required(),
+        }), {friendshipId });
         return this.friendsipsService.deleteFriendship(friendshipId);
     }
 }
