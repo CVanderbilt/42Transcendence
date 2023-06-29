e86f32035208af9ae28f8a9c557b34686b06ee7c
Payit <payit@MacBook-Air-de-Payit.local>
2023-06-29 21:57:16 +0200

    check caducidad del token en frontend y si caduca logout

--- frontend/src/api/baseApi.ts
+++ frontend/src/api/baseApi.ts
@@ -2,6 +2,8 @@ import axios, { AxiosError } from "axios";
 import { API_END_POINT } from "@/config";
 import { throwFromAsync } from "@/utils/utils";
 import { app } from "@/main";
+import jwtDecode from 'jwt-decode';
+import { store } from "@/store/store";
 
 const apiClient = axios.create({
   baseURL: API_END_POINT
@@ -10,6 +12,14 @@ const apiClient = axios.create({
 apiClient.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
+    const decodedToken = jwtDecode<{ exp: number; [key: string]: any }>(token);
+
+    if (decodedToken.exp * 1000 < Date.now()) {
+      store.commit("changeUser", undefined)
+      localStorage.removeItem("token");
+      window.location.href = 'http://localhost:8080/';
+      throwFromAsync(app, "Token expired, loging out")
+    }
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
