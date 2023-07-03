6478b333d172185450d115d0ceb746f572d94f0f
pablomon <pablomonteserin@gmail.com>
2023-07-03 15:54:26 +0200

    Revert "reordenado"
    This reverts commit 9e9de28263fd3eaccbff8ac845e9fc8296cb96e1.

--- srcs/backend/db_init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CONTAINER_NAME=transcendence
-docker stop $CONTAINER_NAME
-docker rm $CONTAINER_NAME
-docker run --name $CONTAINER_NAME  -e POSTGRES_PASSWORD=admin -e POSTGRES_USER=admin -e POSTGRES_DB=ft_transcendence -e TZ=UTC --publish  127.0.0.1:5432:5432 postgres 
-
